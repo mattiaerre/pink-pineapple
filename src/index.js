a595db82d6b15ee52e6f616836f9886b38e7879a
mattia richetto <mrichetto@opentable.com>
2017-04-22 20:01:33 -0700

    add styled wrapper test

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import styled from 'styled-components';
 import App from './containers/App/App';
-
-const Wrapper = styled.div`
-  font-family: Consolas, monaco, monospace;
-  padding: 2em;
-  table {
-    padding: 2em;
-    border-collapse: collapse;
-    border: 1px solid gray;
-  }
-  tbody tr:nth-child(odd) {
-    background: lightpink;
-  }
-  tbody tr:nth-child(even) {
-    background: papayawhip;
-  }
-  th, td {
-    padding: 1em;
-    border: 0px;
-  }
-`;
+import Wrapper from './containers/App/Wrapper';
 
 render(
   <div>
