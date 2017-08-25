080f13cbc97acb045f90eb6e2fb375e8fe28f2ee
mattia richetto <mattia.richetto@gmail.com>
2017-08-25 13:08:02 -0700

    add oc-free-geo-ip-plugin from npm

--- plugins/index.js
+++ plugins/index.js
@@ -1,10 +1,18 @@
 const timezoneOffset = require('./timezone-offset');
+const geoIp = require('oc-free-geo-ip-plugin');
 
 module.exports = (registry) => {
   registry.register(
     {
       name: 'getTimezoneOffset',
       register: timezoneOffset
+    }
+  );
+
+  registry.register(
+    {
+      name: 'getGeoIp',
+      register: geoIp
     }
   );
 };
